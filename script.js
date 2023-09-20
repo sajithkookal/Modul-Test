55b71279c81092fbc90188dc005d91bd7ea724bb
sajithkookal <141862237+sajithkookal@users.noreply.github.com>
2023-09-20 19:18:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
+function preventBack() { window.history.forward(); }  
+setTimeout("preventBack()", 0);  
+window.onunload = function () { null }; 
+
 //----------------Buttons-------------------------------------
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
